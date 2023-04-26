b7a180bdbc352abd943ef299bfa8ad91654ab311
Brandon Crenshaw <crench3@gmail.com>
2023-04-26 15:08:33 -0400

    Section 3

--- globomantics/store/urls.py
+++ globomantics/store/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, re_path
 
 from . import views
+from .views import ElectronicView, ElectronicView2, ElectronicView3, EquipmentView
 
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^\d+', views.detail, name='detail'),
-    re_path(r'^electronics', views.electronics, name='electronics')
+    #re_path(r'^electronics', views.electronics, name='electronics')
+    #re_path(r'^electronics', ElectronicView.as_view(), name='electronics')
+    #re_path(r'^electronics', ElectronicView2.as_view(), name='electronics')
+    #re_path(r'^electronics', ElectronicView3.as_view(), name='electronics')
+    re_path(r'^electronics', EquipmentView.as_view(), name='electronics')
 ]
--- globomantics/store/views.py
+++ globomantics/store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.cache import cache_page
+from django.views.decorators.http import require_http_methods
+from django.http import HttpResponse, HttpResponseNotFound
+from django.core.paginator import Paginator, PageNotAnInteger
 
 def index(request):
     return HttpResponse("Hello")
@@ -8,5 +14,59 @@ def index(request):
 def detail(request):
     return HttpResponse("Hello details")
 
+@csrf_exempt
+@cache_page(900)
+@require_http_methods(["GET"])
 def electronics(request):
-    return HttpResponse("Electronics")
+    items = ("Windows PC", "Apple Mac", "Apple iPhone")
+    if request.method == "GET":
+        paginator = Paginator(items, 2)
+        pages = request.GET.get('page', 1)
+        try:
+            items = paginator.page(pages)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        return render(request, 'store/list.html', {'items': items})
+    elif request.method == "POST":
+        return HttpResponseNotFound("Page Not Found")
+
+
+class ElectronicView(View):
+    def get(self, request):
+        items = ("Windows PC", "Apple Mac", "Apple iPhone")
+        paginator = Paginator(items, 2)
+        pages = request.GET.get('page', 1)
+        self.process()
+        try:
+            items = paginator.page(pages)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        return render(request, 'store/list.html', {'items': items})
+    
+    def process(self):
+        print("We are processing Electronics")
+    
+
+class ElectronicView2(TemplateView):
+    template_name = 'store/list.html'
+    def get_context_data(self, **kwargs):
+        items = ("Windows PC", "Apple Mac", "Apple iPhone")
+        context = {'items': items}
+        return context
+
+class ElectronicView3(ListView):
+    template_name = 'store/list2.html'
+    queryset = ("Windows PC", "Apple Mac", "Apple iPhone")
+    context_object_name = 'items'
+    paginate_by = 2
+
+class ComputersView(ElectronicView):
+    def process(self):
+        print("We are processing Computers")
+
+class MobileView():
+    def process(self):
+        print("We are processing Mobile phones")
+
+class EquipmentView(MobileView, ComputersView):
+    pass
